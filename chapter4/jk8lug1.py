2dc9fb50b36d73084b2e25612e88c05f1ec25299
Tamashiro Souta <n23019@std.it-college.ac.jp>
2023-05-24 07:43:05 +0000

    second

---
+++ chapter4/jk8lug1.py
@@ -0,0 +1,13 @@
+n = input("type a number:")
+n = int(n)
+result = "n is even." if n % 2 == 0 else "n is odd"
+
+
+n = input("type a number:")
+n = int(n)
+result = "n is even." if n % 2 == 0 else "n is odd"
+
+
+n = input("type a number:")
+n = int(n)
+result = "n is even." if n % 2 == 0 else "n is odd"
---
+++ chapter4/zhy8y4m.py
@@ -0,0 +1,8 @@
+def even_odd(x):
+
+
+return 'even' if x % 2 == 0 else "odd"
+
+
+even_odd(2)
+even_odd(3)
